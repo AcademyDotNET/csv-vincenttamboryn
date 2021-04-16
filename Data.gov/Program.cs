53287d04371e3dc51b99b4559672cc78969169cb
vincent tamboryn <vincent.tamboryn@hotmail.com>
2021-04-16 15:22:08 +0200

    txt file writing and layout complete

--- Data.gov/Program.cs
+++ Data.gov/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
 
 namespace Data.gov
 {
@@ -7,8 +13,9 @@ namespace Data.gov
         static void Main(string[] args)
         {
             Console.SetWindowSize(240, 60);
-            string[,]cSV = CSV_reader.readCsv();
+            string[,] cSV = CSV_reader.readCsv();
             Pokedex(cSV);
+            CreateTxT(cSV);
         }
         static void Pokedex<T>(T[,] array)
         {
@@ -54,6 +61,66 @@ namespace Data.gov
             else
             {
                 Console.WriteLine("Array is empty");
+            }
+        }
+        static void CreateTxT(string[,] text)
+        {
+            DateTime datum = DateTime.Today;
+            string date = Convert.ToString(datum);
+            string space ="";
+
+            using (StreamWriter streamWriter = new StreamWriter($@"D:\documenten\Programmeren\C#\CSVs\DemoData.txt"))
+            
+            for (int j = 0; j < text.GetLength(0); j++)
+            {
+                for (int i = 0; i < text.GetLength(1); i++)
+                {
+                    if (i == 1)
+                    {
+                        for(int k = 0; k < (25-text[j,i].Length);k++)
+                        {
+                            space += " ";
+                        }
+                        streamWriter.Write($"{text[j, i]}{space}");
+                        space = "";
+                    }
+                    else if (i == 2)
+                    {
+                        for (int k = 0; k < (12 - text[j, i].Length); k++)
+                        {
+                            space += " ";
+                        }
+                        streamWriter.Write($"{text[j, i]}{space}");
+                        space = "";
+                    }
+                    else if (i == 3)
+                    {
+                        for (int k = 0; k < (12 - text[j, i].Length); k++)
+                        {
+                            space += " ";
+                        }
+                        streamWriter.Write($"{text[j, i]}{space}");
+                        space = "";
+                    }
+                    else if (i == text.GetLength(1) - 2)
+                    {
+                        for (int k = 0; k < (12 - text[j, i].Length); k++)
+                        {
+                            space += " ";
+                        }
+                        streamWriter.Write($"{text[j, i]}{space}");
+                        space = "";
+                    }
+                    else if (i == text.GetLength(1) - 1)
+                    {
+                        streamWriter.Write($"{text[j, i]}");
+                    }
+                    else
+                    {
+                        streamWriter.Write($"{text[j, i]}\t");
+                    }
+                }
+                streamWriter.WriteLine();
             }
         }
     }
